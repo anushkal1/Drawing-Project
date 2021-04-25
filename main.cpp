66340baf86ea6f8c15aa24d74b6ba9ad00f24cd6
isaiahgarcia <73454322+isaiahgarcia@users.noreply.github.com>
2021-04-24 22:58:49 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -65,13 +65,21 @@ void writeOut(ostream& out, ppmR& theWriter, Bird blackBird, Pig piggy) {
 
 /*read command line arguments and write out new ascii file of given size */
 int main(int argc, char *argv[]) {
+	unordered_map<string, color> redBirdColors;
+	redBirdColors["red"] = color(216, 0, 38);
+	redBirdColors["dark red"] = color(170, 0, 30);
+	redBirdColors["desert sand"] = color(227, 196, 166);
 
-	// Colors
+	unordered_map<std::string, color> mainColors = {
+      {"black", color(0, 0, 0) },
+      {"white", color(255, 255, 255) },
+      {"beakColor", color(255, 175, 0)}
+    };
 
-	unordered_map<string, color> mainColors;
-	mainColors["black"] = color(0, 0, 0);
-	mainColors["white"] = color(255, 255, 255);
-	mainColors["beakColor"] = color(255, 175, 0);
+	unordered_map<string, color> yellowBirdColors;
+	yellowBirdColors["main yellow"] = color(242, 221, 0);
+	yellowBirdColors["reflective yellow"] = color(250, 255, 209);
+	yellowBirdColors["red"] = color(180, 53, 0);
 
 	unordered_map<string, color> blackBirdColors;
 	blackBirdColors["tinted black"] = color(68, 68, 68);
@@ -82,26 +90,15 @@ int main(int argc, char *argv[]) {
 	pigColors["outlined green"] = color(5, 132, 12);
 	pigColors["dark green"] = color(38, 115, 42);
 
+	ofstream outFile;
+	int sizeX, sizeY;
+
 	vector<color> niceC;
 	niceC.push_back(color(117, 119, 186));
 	niceC.push_back(color(45, 47, 135));
 	niceC.push_back(color(174, 209, 238));
 	niceC.push_back(color(239, 174, 115));
 	niceC.push_back(color(186, 140, 117));
-
-	/* // :'(
-	unordered_map<string, color> yellowBirdColors;
-	yellowBirdColors["main yellow"] = color(242, 221, 0);
-	yellowBirdColors["reflective yellow"] = color(250, 255, 209);
-	yellowBirdColors["red"] = color(180, 53, 0);
-
-	unordered_map<string, color> redBirdColors;
-	redBirdColors["red"] = color(216, 0, 38);
-	redBirdColors["dark red"] = color(170, 0, 30);
-	redBirdColors["desert sand"] = color(227, 196, 166);
-	*/
-
-	// BlackBird
 
 	ellipse blackBirdBody(vec2(30,30), vec2(20,20), 1.0, mainColors["black"]);
 	vector<ellipse> blackBirdEyes;
@@ -158,33 +155,45 @@ int main(int argc, char *argv[]) {
 	ellipse blackBirdBelly(vec2(30, 40), vec2(8,8), 2.0, blackBirdColors["tinted black"]);
 	Bird blackBird(blackBirdBody, blackBirdEyes, blackBirdBeak, blackBirdTail, blackBirdEyebrows, blackBirdBelly);
 
-	// Pig
-
-	ellipse pigBody(vec2(150,150), vec2(120,80), 1, pigColors["green"]);
+	// CREATING PIG
+	ellipse pigBody(vec2(450,150), vec2(120,80), 1, pigColors["green"]);
 	vector<ellipse> pigEyes;
-	pigEyes.push_back(ellipse(vec2(210,140), vec2(20,20), 2, mainColors["white"]));
-	pigEyes.push_back(ellipse(vec2(90,140), vec2(20,20), 2, mainColors["white"]));
-	pigEyes.push_back(ellipse(vec2(220,140), vec2(8,8), 3, mainColors["black"]));
-	pigEyes.push_back(ellipse(vec2(80,140), vec2(8,8), 3, mainColors["black"]));
+	pigEyes.push_back(ellipse(vec2(510,140), vec2(23,23), 1, pigColors["outlined green"]));
+	pigEyes.push_back(ellipse(vec2(510,140), vec2(20,20), 2, mainColors["white"]));
+	pigEyes.push_back(ellipse(vec2(390,140), vec2(23,23), 1, pigColors["outlined green"]));
+	pigEyes.push_back(ellipse(vec2(390,140), vec2(20,20), 2, mainColors["white"]));
+	pigEyes.push_back(ellipse(vec2(520,140), vec2(8,8), 3, mainColors["black"]));
+	pigEyes.push_back(ellipse(vec2(380,140), vec2(8,8), 3, mainColors["black"]));
 
 	vector<ellipse> pigSnout;
-	pigSnout.push_back(ellipse(vec2(150,160), vec2(38,28), 2, pigColors["outlined green"]));
-	pigSnout.push_back(ellipse(vec2(150,160), vec2(35,25), 3, pigColors["green"]));
-	pigSnout.push_back(ellipse(vec2(130,160), vec2(8,12), 4, mainColors["black"]));
-	pigSnout.push_back(ellipse(vec2(170,160), vec2(8,8), 4, mainColors["black"]));
+	pigSnout.push_back(ellipse(vec2(450,160), vec2(38,33), 2, pigColors["outlined green"]));
+	pigSnout.push_back(ellipse(vec2(450,160), vec2(35,30), 3, pigColors["green"]));
+	pigSnout.push_back(ellipse(vec2(435,160), vec2(8,11), 4, mainColors["black"]));
+	pigSnout.push_back(ellipse(vec2(465,160), vec2(8,8), 4, mainColors["black"]));
 
 	vector<ellipse> pigEars;
-	pigEars.push_back(ellipse(vec2(100,75), vec2(20,20), 2, pigColors["green"]));
-	pigEars.push_back(ellipse(vec2(200,75), vec2(20,20), 2, pigColors["green"]));
-	pigEars.push_back(ellipse(vec2(100,75), vec2(12,12), 3, pigColors["outlined green"]));
-	pigEars.push_back(ellipse(vec2(200,75), vec2(12,12), 3, pigColors["outlined green"]));
+	pigEars.push_back(ellipse(vec2(400,75), vec2(20,20), 2, pigColors["green"]));
+	pigEars.push_back(ellipse(vec2(500,75), vec2(20,20), 2, pigColors["green"]));
+	pigEars.push_back(ellipse(vec2(400,75), vec2(12,12), 3, pigColors["outlined green"]));
+	pigEars.push_back(ellipse(vec2(500,75), vec2(12,12), 3, pigColors["outlined green"]));
 
-	Pig piggy(pigBody, pigEyes, pigSnout, pigEars);
+	vector<Polygon> pigEyebrows;
+	vector<vec2> pigLEyebrow;
+	pigLEyebrow.push_back(vec2(420,100));
+	pigLEyebrow.push_back(vec2(420,110));
+	pigLEyebrow.push_back(vec2(370,115));
+	pigLEyebrow.push_back(vec2(370,110));
 
+	vector<vec2> pigREyebrow;
+	pigREyebrow.push_back(vec2(490,100));
+	pigREyebrow.push_back(vec2(540,110));
+	pigREyebrow.push_back(vec2(540,115));
+	pigREyebrow.push_back(vec2(490,110));
 
+	pigEyebrows.push_back(Polygon(pigLEyebrow, 3, pigColors["dark green"]));
+	pigEyebrows.push_back(Polygon(pigREyebrow, 3, pigColors["dark green"]));
 
-	ofstream outFile;
-	int sizeX, sizeY;
+	Pig piggy(pigBody, pigEyes, pigSnout, pigEars, pigEyebrows);
 
 	if (argc < 4) {
 		cerr << "Error format: a.out sizeX sizeY outfileName" << endl;
@@ -201,34 +210,29 @@ int main(int argc, char *argv[]) {
 	//you will use these
 	vec2 trans1(-1, 1);
 	vec2 trans2(1, 1);
-	// vec2 trans3(-1, -1); // not in use
+	vec2 trans3(-1, -1);
 	vec2 accel(0, 1);
 
 	//loop through and produce more than on image - change to 30
 	for (int i=0; i < 3; i++) {
 
-	  //set up unique filename that conforms to processing movie maker
+	  trans1 = trans1 + accel;
+	  trans2 = trans2 + accel;
+	  trans3 = trans3 + accel;
+
+	  //set up unique filename that conforms to processign movie maker
 	  outFilename.append(argv[3]);
 	  if (i < 10)
 		outFilename.append(to_string(0));
 		outFilename.append(to_string(i));
 		outFilename.append(".ppm");
-
-	  trans1 = trans1 + accel;
-	  trans2 = trans2 + accel;
-	  trans3 = trans3 + accel;
-
 		//open new file
 		outFile.open(outFilename);
 		if (outFile) {
 		  cout << "writing an image of size: " << sizeX << " " << sizeY << " to: " << argv[3] << endl;
 		  theWriter.writeHeader(outFile);
-
-			blackBird.translate(trans1);
-			piggy.translate(trans2);
-
-		  /*
-			for (auto s : theEllipses) {
+		   //uncomment when task 4 is done
+		  /*for (auto s : theEllipses) {
 		  	s->translate(trans1);
 		  }
 		  for (auto p : thePolys) {
